c9f0ded4871dcbc52a95d20222d28003b1db343e
Zhalhou-dev <80689458+Zhalhou-dev@users.noreply.github.com>
2023-05-27 18:12:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,91 +1,44 @@
+# Import Libraries below
 import os
 import cv2
 from flask import Flask, render_template, request, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
 
 
-app = Flask(__name__)
 
 
-@app.route('/')
+# Define flask 
+app=Flask("__name__")
+@app.route('/',methods=['POST'])
+
+
+# Define upload_form() and route the webapp 
+
 def upload_form():
-    return render_template('upload.html')
+	return render_template('upload.html')
 
 
-@app.route('/', methods=['POST'])
+
+
+# Define upload_video() to get video in defined folder and route the webapp  
+@app.route('/index.html',methods=['POST'])
 def upload_video():
-    file = request.files['file']
-    filename = secure_filename(file.filename)
-    file.save(os.path.join('static/', filename))
-
-
-# Code for Project 265 Grayscale start here
-   cv2.VideoCapture('static/'+filename)
-   frame_width=int(source.get(3))
-   frame_height=int(source.get(4))
-   size=(frame_width,frame_height)
-   cv2.VirdeoWriter('static/' + 'blackandwhite.mp4',cv2.VideoWriter_fourcc('mp4v'),fps=30,size,0)
-   try :
-   	while True:
-   		status,frame_image=source.read()
-   		gray=cv2.cvtColor(frame_image,cv2.COLOR_BGR2GRAY)
-   		result.write(gray)
-   		video_file='blackandwhite.mp4'
-   	except:
-   		print('Completed reading all the frames from the video')
+	file=request.files['file']
+	secure_filename(file.filename)
+	file.save(os.path.join('static/',filename))
+	return render_template('upload.html',filename=filename)
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-            
-# Code for Project 265 end here
-
-    return render_template('upload.html', filename=filename)
-
-
-
-# Code for Project 265 download funcion starts here
-@app.route('/download')
-def download_file():
-	converted_video_path="static/blackandwhite.mp4"
-	return send_file(converted_video_path, as_attachment=True)
-
-
-
-
-
-
-# Code for Project 265 download funcion ends here
-
-
-
-@app.route('/display/<filename>')
+# Define display_video() to Display video in defined folder and route the webapp  
+@app.route('/display/')
 def display_video(filename):
-    return redirect(url_for('static', filename=filename))
+	return render_template('upload.html',filename=filename)
+
+
 
 
 if __name__ == "__main__":
